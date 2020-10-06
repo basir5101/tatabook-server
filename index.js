a3cafa12507e035b1d951f42c20a2e0730a7b371
basir5101 <67514986+basir5101@users.noreply.github.com>
2020-10-06 13:37:46 +0600

    post request add

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+const port = 5000
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jo990.mongodb.net/${process.env.DB_USER}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("tatabook").collection("tatabook_user");
+      console.log('db connected')
+ 
+
+
+    app.post('/user', (req, res) =>{
+        console.log(req.body)
+        collection.insertOne(req.body)
+        .then(result =>{
+            if(result.insertedCount > 0){
+                res.send(result)
+            }
+        })
+    })
+
+
+
+
+});
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port)
